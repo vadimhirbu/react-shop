68498d163de22b6764771c6660e048482da648f7
Vadim Hirbu <hirbu.vadim@gmail.com>
2020-04-27 13:50:15 +0300

    Move our directory and shop data into its own reducer. Create respective selectors and update corresponding components to use redux flow for directory/shop.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import SHOP_DATA from "./shop.data";
+import {Route} from 'react-router-dom';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+const ShopPage = ({match}) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render() {
-    const {collections} = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map( ({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    );
-  }
-
-}
-
-export  default ShopPage;
+export default ShopPage;
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -5,7 +5,6 @@ const INITIAL_STATE = {
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
-  console.log(action.type, 'action.type');
   switch (action.type) {
     case UserActionTypes.SET_CURRENT_USER:
       return {
